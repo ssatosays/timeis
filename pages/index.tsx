f2b4b52e8792aa949cf521877c938d5f88102eb6
ssatosays <ssatosays@gmail.com>
2022-11-23 23:07:51 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,24 @@
-// import Link from 'next/link'
+import { useState, useEffect } from 'react';
 
 export default function Home() {
+  const [now, setDate] = useState(new Date())
+
+  useEffect(() => {
+    const curr = setInterval(() => {
+      setDate(new Date());
+    }, 1000)
+    return () => {
+      clearInterval(curr)
+    }
+  }, [])
+
   return (
     <div>
-      <h1>21:45:45</h1>
+      <h1 suppressHydrationWarning>
+        {now.getHours().toString().padStart(2, '0')}:
+        {now.getMinutes().toString().padStart(2, '0')}:
+        {now.getSeconds().toString().padStart(2, '0')}
+      </h1>
     </div>
   )
 }
