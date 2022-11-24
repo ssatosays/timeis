aaa464588a2213728f8c11fcce2ad5159a818af0
ssatosays <ssatosays@users.noreply.github.com>
2022-11-24 13:51:50 +0000

    deploy: 0869ba9df829cc0127585cfdb8f3341f7d894268

---
+++ _next/static/J1ixxQHb7mjHQBQHyZGBJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/722-b6dac601e8f5a11a.js","static/css/39b6dbd0c6c9a76e.css","static/chunks/pages/index-31afb74ec3759326.js"],"/_error":["static/chunks/pages/_error-dfcfa5bb62767c20.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
